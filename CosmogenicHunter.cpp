d3c0cc86545de6e8eb7a61e7e0dd89f50a4079fd
Dakeryas <mrdakeryas@gmail.com>
2015-11-01 22:15:48 +0100

    Test writing to binary files and reading them back with Cereal

--- CosmogenicHunter.cpp
+++ CosmogenicHunter.cpp
@@ -7,31 +7,53 @@
 #include "Candidate.hpp"
 #include "Shower.hpp"
 
+namespace CsHt = CosmogenicHunter;
 namespace bpo = boost::program_options;
+using MuonShower = CsHt::Shower<CsHt::Muon, CsHt::Neutron>;
 
 void hunt(unsigned runNumber, boost::filesystem::path outputPath){
   
-  CosmogenicHunter::Event event(31.5e9, 30, 9873528);
+  CsHt::Event event(31.5e9, 30, 9873528);
   
-  CosmogenicHunter::Window<CosmogenicHunter::Event> window(30e9, 2e9);
+  CsHt::Window<CsHt::Event> window(30e9, 2e9);
   window.emplaceEvent(31e9, 30, 9873528);
   window.pushBackEvent(event);
   
-  CosmogenicHunter::Point<float> point1(1, 1, 2);
-  CosmogenicHunter::Point<float> point2(3, 2, 3);
-  CosmogenicHunter::Segment<float> segment(point1, point2);
+  CsHt::Point<float> point1(1, 1, 2);
+  CsHt::Point<float> point2(3, 2, 3);
+  CsHt::Segment<float> segment(point1, point2);
   
-  CosmogenicHunter::Muon muon(event, 5e4, 8e4, segment);
-  CosmogenicHunter::Neutron neutron(event, point2);
-  CosmogenicHunter::Candidate candidate(event, point2, 3);
-  CosmogenicHunter::Shower<CosmogenicHunter::Muon, CosmogenicHunter::Neutron> muonShower(muon, 2e9);
+  CsHt::Muon muon(event, 5e4, 8e4, segment);
+  CsHt::Neutron neutron(event, point2);
+  CsHt::Candidate candidate(event, point2, 3);
+  MuonShower muonShower(muon, 2e9);
   muonShower.pushBackFollower(neutron);
-  muonShower.emplaceFollower(event, CosmogenicHunter::Point<float>(5, 5, 5));
-  muonShower.emplaceFollower(33e9, 8, 106785, CosmogenicHunter::Point<float>(1, 5, 5));
-  std::cout<<muonShower<<std::endl;
+  muonShower.emplaceFollower(event, CsHt::Point<float>(5, 5, 5));
+  muonShower.emplaceFollower(33e9, 8, 106785, CsHt::Point<float>(1, 5, 5));
+  CsHt::Window<MuonShower> muonShowers(muonShower.getTriggerTime(), 20e9);
+  for(unsigned k = 0; k  < 10*30*20; ++k) muonShowers.pushBackEvent(muonShower);
   
-  std::ofstream outputStream(outputPath.string());
-  outputStream<<window<<std::endl;
+  {
+    
+    std::ofstream outputStream(outputPath.string(), std::ios::binary);
+    cereal::BinaryOutputArchive outputArchive(outputStream);
+    outputArchive(event);
+    outputArchive(neutron);
+    outputArchive(muon);
+    outputArchive(muonShowers);
+  
+  }
+  
+  std::ifstream inputStream(outputPath.string(), std::ios::binary);
+  cereal::BinaryInputArchive inputArchive(inputStream);
+  
+  CsHt::Window<MuonShower> muonShowersInput;
+  CsHt::Event inputEvent;
+  inputArchive(inputEvent);
+  inputArchive(CsHt::Neutron());
+  inputArchive(CsHt::Muon());
+  inputArchive(muonShowersInput);
+//   std::cout<<muonShowersInput<<std::endl;
   
   Message::SetLevelMSG(DC::kMERROR);
   
@@ -43,7 +65,7 @@ void hunt(unsigned runNumber, boost::filesystem::path outputPath){
   Processor::GetME()->UncancelInfoCapsule_Tree(DC::kRecoBAMAIT);
   Processor::GetME()->UncancelInfoCapsule_Tree(DC::kRecoMuHamIDIT);
   Processor::GetME()->SaveOutput(false);
-  Processor::GetME()->Run();
+//   Processor::GetME()->Run();
   
 }
 
