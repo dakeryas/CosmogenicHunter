35ed9de9191f92685095a57f7681b162de5a35b8
Dakeryas <mrdakeryas@gmail.com>
2015-11-13 17:57:27 +0100

    Added a toString(flavour) function to convert Flavour's to string on top of printing

--- include/Flavour.hpp
+++ include/Flavour.hpp
@@ -7,6 +7,7 @@
 namespace CosmogenicHunter{
 
   enum class Flavour : char {Muon, Neutron, Candidate, Other};
+  const std::string& toString(Flavour flavour);
   std::ostream& operator<<(std::ostream& output, Flavour flavour);
   
 }
--- src/Flavour.cpp
+++ src/Flavour.cpp
@@ -3,9 +3,16 @@
 namespace CosmogenicHunter{
 
   std::map <Flavour, std::string> flavourMap = {{Flavour::Muon, "Muon"}, {Flavour::Neutron, "Neutron"}, {Flavour::Candidate, "Candidate"}, {Flavour::Other, "Other"}};
+  
+  const std::string& toString(Flavour flavour){
+    
+    return flavourMap[flavour];
+
+  }
+
   std::ostream& operator<<(std::ostream& output, Flavour flavour){
     
-    output<<flavourMap[flavour];
+    output<<toString(flavour);
     return output;
 
   }
