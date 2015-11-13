7cb6d229dabf0a0b903b5f7ec372288048db8d5e
Dakeryas <mrdakeryas@gmail.com>
2015-11-13 13:59:05 +0100

    Added double triggerTime as member of Entry<T>

--- include/Entry.hpp
+++ include/Entry.hpp
@@ -6,26 +6,36 @@
 namespace CosmogenicHunter{
   
   template <class T>
-  struct Entry{
+  struct Entry{//public for ROOT TTree's branches address
     
+    double triggerTime;
     T IVCharge;
     T IDCharge;
     T energy;
     unsigned identifier;
     Entry() = default;
-    Entry(T IVCharge, T IDCharge, T energy, unsigned identifier):IVCharge(IVCharge),IDCharge(IDCharge),energy(energy),identifier(identifier){}
+    Entry(double triggerTime, T IVCharge, T IDCharge, T energy, unsigned identifier);
     
   };
   
   template <class T>
+  Entry<T>::Entry(double triggerTime, T IVCharge, T IDCharge, T energy, unsigned identifier)
+  :triggerTime(triggerTime),IVCharge(IVCharge),IDCharge(IDCharge),energy(energy),identifier(identifier){
+      
+  }
+  
+  template <class T>
   std::ostream& operator<<(std::ostream& output, const Entry<T>& entry){
       
-    output<<std::setw(12)<<std::right<<entry.IVCharge<<" | "<<std::setw(12)<<std::right<<entry.IDCharge<<" | "<<std::setw(8)<<std::right<<entry.energy<<" | "<<std::setw(8)<<std::right<<entry.identifier;
+    output<<std::setw(12)<<std::left<<entry.triggerTime<<" | "
+      <<std::setw(10)<<std::left<<entry.IVCharge<<" | "
+      <<std::setw(10)<<std::left<<entry.IDCharge<<" | "
+      <<std::setw(8)<<std::left<<entry.energy<<" | "
+      <<std::setw(8)<<std::left<<entry.identifier;
     return output;
       
   };
   
 }
-
 
 #endif
