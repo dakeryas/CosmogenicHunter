3dab566fff503b2904c89ec93a7505ef2cd43010
Dakeryas <mrdakeryas@gmail.com>
2015-11-13 15:42:48 +0100

    Simple CsHt::Utility::getRunNumber function taking the stem of a filename as argument

---
+++ include/Utility.hpp
@@ -0,0 +1,17 @@
+#ifndef UTILITY_H
+#define UTILITY_H
+
+#include <regex>
+
+namespace CosmogenicHunter{
+
+  namespace Utility{
+    
+    unsigned getRunNumber(const std::string& targetStem);
+    
+  }
+
+}
+
+
+#endif
---
+++ src/Utility.cpp
@@ -0,0 +1,20 @@
+#include "Utility.hpp"
+
+namespace CosmogenicHunter{
+
+  namespace Utility{
+    
+    unsigned getRunNumber(const std::string& targetStem){
+
+      unsigned runNumber{};
+      std::regex regex("\\.([0-9]{6,7})");
+      std::smatch regexMatches;
+      if(std::regex_search(targetStem, regexMatches, regex)) runNumber = std::stoul(regexMatches.str(1));
+      
+      return runNumber;
+      
+    }
+    
+  }
+
+}
