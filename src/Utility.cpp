b6cc89d4963db3858608edb1528a1534c26f2045
Dakeryas <mrdakeryas@gmail.com>
2015-12-09 15:14:53 +0100

    Allow run numbers to be extracted from filename when they only have 5 significant digits

--- src/Utility.cpp
+++ src/Utility.cpp
@@ -7,7 +7,7 @@ namespace CosmogenicHunter{
     unsigned getRunNumber(const std::string& targetStem){
 
       unsigned runNumber{};
-      std::regex regex("\\.([0-9]{6,7})");
+      std::regex regex("\\.([0-9]{5,7})");
       std::smatch regexMatches;
       if(std::regex_search(targetStem, regexMatches, regex)) runNumber = std::stoul(regexMatches.str(1));
       
