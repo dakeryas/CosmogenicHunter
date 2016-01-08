40b7670eaac8b4eb22c981bc5a4d1c065a647bb9
Dakeryas <mrdakeryas@gmail.com>
2016-01-08 19:12:43 +0100

    Better printing of InnerVetoData

--- include/InnerVetoData.hpp
+++ include/InnerVetoData.hpp
@@ -17,11 +17,13 @@ namespace CosmogenicHunter{
   template <class T>
   std::ostream& operator<<(std::ostream& output, const InnerVetoData<T>& innerVetoData){
       
+    auto formerPrecision = output.precision();
     output<<std::fixed;
     
-    output<<std::setw(9)<<std::right<<innerVetoData.charge<<" | "
+    output<<std::setprecision(0)<<std::setw(9)<<std::right<<innerVetoData.charge<<" | "
       <<std::setw(3)<<std::right<<innerVetoData.numberOfHitPMTs;
-      
+    
+    output<<std::setprecision(formerPrecision);
     return output;
     
   };
