3b8197d5c1867c66fa8b7854706711c06654d148
Dakeryas <mrdakeryas@gmail.com>
2016-02-01 18:12:49 +0100

    Added InnerDetectorData<T> POD to save the startTime[3] and charge[3]

---
+++ include/InnerDetectorData.hpp
@@ -0,0 +1,35 @@
+#ifndef COSMOGENIC_INNER_DETECTOR_DATA_H
+#define COSMOGENIC_INNER_DETECTOR_DATA_H
+
+#include "PositionData.hpp"
+
+namespace CosmogenicHunter{
+  
+  template <class T>
+  struct InnerDetectorData{
+    
+    T charge[3];//mini data comes last
+    T startTime[3];//first is average
+    PositionData<T> positionData;
+    InnerDetectorData() = default;
+    
+  };
+  
+  template <class T>
+  std::ostream& operator<<(std::ostream& output, const InnerDetectorData<T>& innerDetectorData){
+      
+    auto formerPrecision = output.precision();
+    output<<std::fixed;
+    
+    output<<std::setprecision(0)<<std::setw(9)<<std::right<<innerDetectorData.charge[2]<<" | "
+      <<std::setw(3)<<std::right<<innerDetectorData.startTime[0]<<" | "
+      <<innerDetectorData.positionData;
+    
+    output<<std::setprecision(formerPrecision);
+    return output;
+    
+  };
+  
+}
+
+#endif
